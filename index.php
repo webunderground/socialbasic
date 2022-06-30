045f13f672b8b59307fe0a5c485ac405053afe1d
webunderground <101758687+webunderground@users.noreply.github.com>
2022-06-30 15:57:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 <?php 
   // Se conecta al SGBD 
-  if(!($conexion = mysql_connect("localhost", "root", "lolita1873"))) 
+  if(!($conexion = mysql_connect("localhost", "root", "password"))) 
     die("Error: No se pudo conectar");
  
   // Selecciona la base de datos 
