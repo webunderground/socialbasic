f98f719acad16856a058515127aceee975cd1a97
webunderground <101758687+webunderground@users.noreply.github.com>
2022-06-30 15:53:27 -0500

    Add files via upload

---
+++ sql/muro.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL auto_increment,
+  `usuario` varchar(20) NOT NULL,
+  `comentario` varchar(3000) NOT NULL,
+  `fecha` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
