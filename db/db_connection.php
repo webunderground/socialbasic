4d88e8cfb20150edfc6856c9a5cc50ec95fa977a
webunderground <101758687+webunderground@users.noreply.github.com>
2022-06-30 15:48:11 -0500

    Update db_connection.php

--- db/db_connection.php
+++ db/db_connection.php
@@ -3,7 +3,7 @@ require_once('mysql.php');
 
 $serverhost = "localhost";
 $serveruser = "root";
-$serverpwd  = "lolita1873";
+$serverpwd  = "password";
 $dbname     = "fce";
 
 $MyDb       = new cMysqlDB($serverhost,$serveruser,$serverpwd,$dbname);
